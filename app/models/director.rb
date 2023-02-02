cbc51687fb874015d7c228ae73e16a0676b6635e
nickjo24 <njohnso2@chicagobooth.edu>
2023-02-02 03:00:35 +0000

    Set up models for each of the tables

---
+++ app/models/director.rb
@@ -0,0 +1,8 @@
+class Director < ApplicationRecord
+#Don't need to "require Director" in order to use in other parts of application.
+#Rails does that for us automatically.
+
+#Each record in the database table represents an instance of this class we've created.
+#AMAZING!
+
+end
