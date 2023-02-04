002403088616f885b473e88ea4c42892b4bac626
nickjo24 <njohnso2@chicagobooth.edu>
2023-02-04 22:54:43 +0000

    Almost finished details page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,6 +6,15 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb"})
   end
 
+  def director_details
+
+    id = params.fetch("id").to_i
+
+    @director = Director.where({ :id => id}).at(0)
+
+    render({ :template => "director_templates/details.html.erb"})
+  end
+
   def eldest
     @eldest_director = Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
     @eldest_name = @eldest_director.name
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index"})
   get("/directors/:id", { :controller => "directors", :action => "director_details"})
+
+  #Stopped at 1:16:36
 end
